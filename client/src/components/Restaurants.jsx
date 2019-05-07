9e4ee1fb083ff3b4f11d2b7b7c176e1bbc219049
Adrienne Lim <adriennelim.mc@gmail.com>
2019-05-07 12:17:57 -0700

    Add some images as placeholders for actual functionality for Restaurants

--- client/src/components/Restaurants.jsx
+++ client/src/components/Restaurants.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Restaurants = (props) => (
     <div>
-        Restaurant feature coming soon!
+        <input type="text" className="search-bar" value=" Seattle" />
+        <button className="search-button">Search</button>
+        <div className="restaurant-map-container">
+            <div>
+                <h2 className="restaurant-category-title">Top Restaurants in Seattle, WA</h2>
+                <img src="https://i.imgur.com/OG2Ngiv.png" alt="Placeholder image of restaurants" />
+            </div>
+            <div>
+                <img src="https://i.imgur.com/F6worlk.png" alt="Placeholder image of google maps" />
+            </div>
+        </div>
     </div>
 )
 
