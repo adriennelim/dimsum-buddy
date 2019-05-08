3a50818681256a63441e77a23c63106f2aae2835
Adrienne Lim <adriennelim.mc@gmail.com>
2019-05-07 19:09:15 -0700

    Added some elements for restaurant mockup

--- client/src/components/Restaurants.jsx
+++ client/src/components/Restaurants.jsx
@@ -5,12 +5,12 @@ const Restaurants = (props) => (
         <input type="text" className="search-bar" value=" Seattle" />
         <button className="search-button">Search</button>
         <div className="restaurant-map-container">
-            <div>
+            <div className="restaurant-container">
                 <h2 className="restaurant-category-title">Top Restaurants in Seattle, WA</h2>
-                <img src="https://i.imgur.com/OG2Ngiv.png" alt="Placeholder image of restaurants" />
+                <img className="mock-restaurants" src="https://i.imgur.com/OG2Ngiv.png" alt="Placeholder image of restaurants" />
             </div>
-            <div>
-                <img src="https://i.imgur.com/F6worlk.png" alt="Placeholder image of google maps" />
+            <div className="map-container">
+                <img className="mock-map" src="https://i.imgur.com/F6worlk.png" alt="Placeholder image of google maps" />
             </div>
         </div>
     </div>
